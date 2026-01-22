c6fbfd595d902554f22bc93a38aa4d880e64e372
Herb Caudill <herb@devresults.com>
2026-01-22 19:16:40 +0100

    index.ts: add block comments to functions and if statement

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,10 +1,15 @@
 import { program } from "./cli.js"
 
+/**
+ * Parse and execute the CLI program with command-line arguments.
+ */
 export const run = () => {
   program.parse(process.argv)
 }
 
-// Run if called directly
+/**
+ * Run if called directly as a script.
+ */
 if (import.meta.url === `file://${process.argv[1]}`) {
   run()
 }
