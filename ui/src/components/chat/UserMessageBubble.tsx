5b74253b90d5be603f9bdb6f420410eff43b70bd
Herb Caudill <herb@devresults.com>
2026-01-22 19:36:56 +0100

    UserMessageBubble: code style fixes

--- ui/src/components/chat/UserMessageBubble.tsx
+++ ui/src/components/chat/UserMessageBubble.tsx
@@ -1,6 +1,9 @@
 import { cn } from "@/lib/utils"
 import type { TaskChatMessage } from "@/types"
 
+/**
+ * Renders a user message bubble in the task chat interface.
+ */
 export function UserMessageBubble({ message, className }: Props) {
   return (
     <div className={cn("flex justify-end py-2 pr-4 pl-12", className)}>
@@ -12,6 +15,8 @@ export function UserMessageBubble({ message, className }: Props) {
 }
 
 type Props = {
+  /** The user message to display */
   message: TaskChatMessage
+  /** Optional CSS class name to apply to the container */
   className?: string
 }
