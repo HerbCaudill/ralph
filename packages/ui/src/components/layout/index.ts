a01b6c58e77a2572248e7add85da3076dbac3891
Herb Caudill <herb@devresults.com>
2026-02-03 22:35:31 +0100

    ThemeToggle: add component for cycling theme modes (r-oc7b7.5)

--- packages/ui/src/components/layout/index.ts
+++ packages/ui/src/components/layout/index.ts
@@ -1,3 +1,4 @@
 export { Header, type HeaderProps } from "./Header"
 export { Logo } from "./Logo"
 export { SettingsDropdown, type SettingsDropdownProps } from "./SettingsDropdown"
+export { ThemeToggle } from "./ThemeToggle"
